92715d74e7aab914648caa6ccfb45cb1858399f6
Arnav Gupta <arnav.gu03@gmail.com>
2026-02-23 00:09:40 -0800

    Added preprocessing bash script to collect data, refined dir structure, added .gitignore

---
+++ src/scripts/preprocess.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Starting Daily ETL Pipeline"
+
+DATA_URL="https://downloads.psl.noaa.gov/Datasets/cpc_global_temp/tmax.2026.nc"
+LOCAL_FILE="data/raw_data.nc"
+
+echo "Fetching daily NetCDF data..."
+curl -s -o $LOCAL_FILE $DATA_URL
+
+if [ -s "$LOCAL_FILE" ]; then
+    echo "Validation Passed: File downloaded."
+    # Run the Python script
+    python src/scripts/pipeline.py
+else
+    echo "Validation Failed: File empty."
+    exit 1
+fi
